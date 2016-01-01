89db59209a55d852716e6f0a0260255c33b4ec32
stephaneuler <stephan.euler@mnd.thm.de>
2016-01-01 16:33:20 +0100

    comment

--- java/ExecutorListener.java
+++ java/ExecutorListener.java
@@ -1,10 +1,24 @@
 package jserver;
 
+/**
+ * The listener interface for receiving compilation and execution events. The
+ * class that is interested in processing such an event implements this
+ * interface, and the object created with that class is registered with a
+ * CodeExecutor, using the executor's addListener method.
+ * 
+ * @author Euler
+ * 
+ */
+
 public interface ExecutorListener {
 	public void startCompilation();
+
 	public void failedCompilation();
+
 	public void endCompilation();
+
 	public void startExecution();
+
 	public void endExecution();
 
 }
