b8db1a5856ccdd7d172affea0782b7d5eee68f51
Stephan Euler <stephan.euler@mnd.thm.de>
2020-12-03 10:27:46 +0100

    Update BoardPainter.java

--- demos/BoardPainter.java
+++ demos/BoardPainter.java
@@ -18,107 +18,119 @@ import jserver.BoardClickListener;
 import jserver.XSendAdapter;
 import plotter.Graphic;
 
-/**
- * A demo application using BoS to implement a simple painter. 
- * 
- * @author Euler
- *
- */
+// ****************************************************************
+// Demo fuer GUI-Moeglichkeiten mit BoS
+// Version Wann      Wer  Was
+// 1.0     20.12.03  SE   Erste Versio
+//****************************************************************
+
 public class BoardPainter implements BoardClickListener {
-	private Board board;
-	private Graphic graphic;
 	private XSendAdapter xsend;
 	private JTextField indexField = new JTextField();
-	private int farben[] = {XSendAdapter.GREEN, XSendAdapter.YELLOW, XSendAdapter.RED };
-	private String[] farbNamen = {"Grün", "Gelb", "Rot" };
+	private int farben[] = { XSendAdapter.GREEN, XSendAdapter.YELLOW, XSendAdapter.RED };
+	private String[] farbNamen = { "Gruen", "Gelb", "Rot" };
 	private int zeichenFarbe = XSendAdapter.BLUE;
+	private int standardfarbe = XSendAdapter.LIGHTGRAY;
+	private int groesse = 10;
 
 	public static void main(String[] args) {
 		BoardPainter e = new BoardPainter();
 		e.starten();
-
 	}
 
 	void starten() {
-		board = new Board();
-		graphic = board.getGraphic();
+		Board board = new Board();
+		Graphic graphic = board.getGraphic();
 		xsend = new XSendAdapter(board);
-		
-		// Standardfarbe
-		xsend.groesse(20, 20);
-		xsend.farben(XSendAdapter.LIGHTGRAY);
+		xsend.groesse(groesse, groesse );
+		xsend.farben(standardfarbe);
 
+		graphic.setTitle("Board Painter Version 1.0");
 		board.addClickListener(this);
+		
+		addButtons(graphic);
+		addPositionInput(graphic);
+		addButtonGroup( graphic );
+		addMenue( graphic );
 
-		JButton faerbeButton = new JButton("färben");
+		graphic.pack();
+		graphic.repaint();
+	}
+
+	private void addButtons(Graphic graphic ) {
+		JButton faerbeButton = new JButton("faerben");
 		faerbeButton.addActionListener(new ActionListener() {
-
+	
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String text = indexField.getText();
-				if( text.isEmpty() ) {
+				if (text.isEmpty()) {
 					JOptionPane.showMessageDialog(graphic, "Bitte Wert eintragen", "Leeres Feld",
 							JOptionPane.ERROR_MESSAGE);
 					return;
 				}
 				int index = Integer.parseInt(text);
-				xsend.farbe(index, XSendAdapter.ORANGE);
+				xsend.farbe(index, zeichenFarbe );
 			}
 		});
-
-		JButton loescheButton = new JButton("löschen");
+	
+		JButton loescheButton = new JButton("loeschen");
 		loescheButton.addActionListener(new ActionListener() {
-
+	
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				xsend.farben(XSendAdapter.LIGHTGRAY);
 			}
 		});
-
+	
 		graphic.addBottomComponent(loescheButton);
 		graphic.addBottomComponent(faerbeButton);
-		graphic.addBottomComponent(new JLabel("pos:"));
+	}
+
+	private void addPositionInput(Graphic graphic) {
+		graphic.addBottomComponent(new JLabel("pos (n):"));
 		graphic.addBottomComponent(indexField);
-		
+	}
+
+	private void addButtonGroup(Graphic graphic) {
 		graphic.addEastComponent(new JLabel("Click-Farben"));
 		ButtonGroup group = new ButtonGroup();
-		for( int f=0; f<farben.length; f++ ) {
-			JRadioButton farbWaehler = new JRadioButton( farbNamen[f]);
-			farbWaehler.setActionCommand(""+farben[f]);
+		for (int f = 0; f < farben.length; f++) {
+			JRadioButton farbWaehler = new JRadioButton(farbNamen[f]);
+			farbWaehler.setActionCommand("" + farben[f]);
 			group.add(farbWaehler);
 			farbWaehler.addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					zeichenFarbe = Integer.parseInt(e.getActionCommand());
-					xsend.statusText("Farbe gewechselt: " + Integer.toHexString(zeichenFarbe ) );
+					xsend.statusText("Farbe gewechselt: " + Integer.toHexString(zeichenFarbe).toUpperCase() );
 				}
 			});
 			graphic.addEastComponent(farbWaehler);
 		}
-		
+
+	}
+
+	private void addMenue(Graphic graphic) {
 		JMenu hilfeMenu = new JMenu("Hilfe");
 		graphic.addExternMenu(hilfeMenu);
-		JMenuItem ueber = new JMenuItem("Über");
+		JMenuItem ueber = new JMenuItem("Ueber");
 		ueber.addActionListener(new ActionListener() {
 
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					JOptionPane.showMessageDialog(graphic, "Testanwendung Version 0.0", "Über",
-							JOptionPane.INFORMATION_MESSAGE);
-				}
-			});
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(graphic, "Testanwendung Version 0.0", "Ueber",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 		hilfeMenu.add(ueber);
-
-
-		graphic.pack();
-		graphic.repaint();
 	}
 
 	@Override
 	public void boardClick(BoardClickEvent info) {
 		System.out.println(info);
-		xsend.farbe2( info.getX(), info.getY(), zeichenFarbe );
+		xsend.farbe2(info.getX(), info.getY(), zeichenFarbe);
 	}
 
 }
